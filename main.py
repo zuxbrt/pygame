348ff657c46436fb307f26c13c443948986fdf6f
zuxbrt <zmuhovic97@gmail.com>
2020-11-07 18:21:58 +0100

    Added window

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-## https://linuxize.com/post/how-to-create-python-virtual-environments-on-ubuntu-18-04/
-print('hello');
+import pygame
+from pygame.locals import *
+
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = True
+        self.size = self.width, self.height = 640, 400
+
+
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        pygame.display.set_caption('prozor')
+        self._running = True
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+if __name__ == "__main__": 
+    theApp = App();
+    theApp.on_execute()
